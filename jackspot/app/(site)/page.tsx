bf65bd97b5a28f4b44a7e7ec4e5965a134cd46db
John John Penafiel <johnjohnpenafiel@hotmail.com>
2024-07-21 21:54:10 -0400

    Checkpoint 44:02

--- jackspot/app/(site)/page.tsx
+++ jackspot/app/(site)/page.tsx
@@ -1,9 +1,13 @@
+import Header from "@/components/Header";
+
 
 export default function Home() {
   return (
     <>
       <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
-        
+        <Header>
+
+        </Header>
       </div> 
     </>
   );
