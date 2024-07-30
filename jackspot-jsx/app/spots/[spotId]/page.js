cd268144ea91434ee70f0a59c103089a9d907232
John John Penafiel <johnjohnpenafiel@hotmail.com>
2024-07-30 01:12:21 -0400

    UI update

--- jackspot-jsx/app/spots/[spotId]/page.js
+++ jackspot-jsx/app/spots/[spotId]/page.js
@@ -30,23 +30,25 @@ function SpotPage() {
     )
   }
 
-  
+
   return (
 
     <div className="bg-neutral-400 rounded-lg h-full w-full  overflow-hidden overflow-y-auto transition">
       <Header>
-        <div className='mt-30'>
+        <div className='mt-10'>
           <div className='flex flex-col md:flex-row items-center gap-x-5'>
             <div className='lg:h-60 lg:w-60 flex-shrink-0'>
-              <img alt='Spot' className='object-cover h-60 w-60 rounded-lg' src={spot.image}/>
-              <h2 className='text-2xl text-neutral-700 semi-bold mt-4'>Address:</h2>
+              {(!spot.image ? null : 
+                <img alt='Spot' className='object-cover h-60 w-60 rounded-lg' src={spot.image}/>
+              )}
+              {/* <h2 className='text-2xl text-neutral-700 semi-bold mt-4'>Address:</h2> */}
               <p className='text-xl text-neutral-100'>{spot.address}</p>
             </div>
             <div className='flex flex-col gap-y-2 mt-4 md:mt-0'>
               <p className='hidden md:block font-semibold text-neutral-600 text-xl'>
                 {spot.type}
               </p>
-              <h1 className='text-white text-4xl sm:text-5xl lg:text-7xl font-bold' >
+              <h1 className=' flex flex-col text-white text-4xl sm:text-5xl lg:text-7xl font-semibold' >
                 {spot.name}
               </h1>
             </div>
--- jackspot-jsx/components/SidebarItem.jsx
+++ jackspot-jsx/components/SidebarItem.jsx
@@ -7,7 +7,7 @@ function SidebarItem({ icon: Icon, label, active, href}) {
     return (
         <Link href={href} className={twMerge("flex flex-row h-auto items-center w-full gap-x-4 text-md font-medium cursor-pointer hover:text-neutral-300/75 transition text-neutral-700 py-1")}>
             <Icon size={26} />
-            <p className="truncate w-full">{label}</p>
+            <p className="truncate w-full font-normal">{label}</p>
         </Link>
     )
 }
